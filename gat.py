00b81d291d2edc3071ea10cb7c2401d2e41a90cd
Piet Cordemans <cordemans.piet@gmail.com>
2022-08-23 23:01:28 +0200

    finds common commit still has bug

--- gat.py
+++ gat.py
@@ -1,12 +1,17 @@
 class Gat:
+    """Gat is a version source control system which looks a bit like Git"""
+
     def __init__(self):
+        """Upon calling the constructor a main branch is created with an initial commit"""
         self.__branches = {'main' : _Branch(_Commit('initial commit','system'))}
         self.__current = 'main'
 
     def status(self):
+        """Show the current branch, with the current commit"""
         return self.__current + ' >> ' + str(self.__branches[self.__current].head())
 
     def commit(self, message, committer, content):
+        """Adds a commit to the current branch"""
         self.__currentBranch().addCommit(
             _Commit(message, committer, content, self.__currentBranch().head()))
 
@@ -14,13 +19,16 @@ class Gat:
         return self.__branches[self.__current]
 
     def createBranch(self, name):
-        self.__branches[name] = self.__currentBranch()
+        """Creates a new branch with a given name, pointing to the current commit"""
+        self.__branches[name] = _Branch(self.__currentBranch().head())
 
     def checkout(self, name):
+        """Switches between branches"""
         if name in self.__branches:
             self.__current = name
 
     def log(self):
+        """Returns a string of all commits in the current branch"""
         return self.__collectAllCommits(self.__currentBranch().head())
 
     def __collectAllCommits(self, cursor):
@@ -30,9 +38,18 @@ class Gat:
             return str(cursor) + '\n' + self.__collectAllCommits(cursor.getPrevious())
 
     def findCommonCommit(self, otherBranchName):
-        pass
+        """Finds the common commit between the current branch and the other branch"""
+        return str(self.__findCommonCommit(self.__branches[otherBranchName].head()))
+
+    def __findCommonCommit(self, cursor):
+        if self.findCommit(cursor.getMessage()):
+            return cursor            
+        else:
+           self.__findCommonCommit(cursor.getPrevious())
+
 
     def findCommit(self, message):
+        """Returns True if the commit identified by the message can be found in the current branch"""
         return self.__findCommit(message, self.__currentBranch().head())
 
     def __findCommit(self, message, cursor):
--- test_gat.py
+++ test_gat.py
@@ -17,11 +17,44 @@ def test_branch():
     vcs.checkout('dev')
     assert(vcs.status() == 'dev >> piet : first commit')
 
+def test_commitAfterBranch():
+    vcs = Gat()
+    vcs.commit('first commit', 'piet', 'somethings')
+    vcs.createBranch('dev')
+    vcs.checkout('dev')
+    vcs.commit('2nd commit', 'nico', 'other things')
+    assert(vcs.status() == 'dev >> nico : 2nd commit')
+
+def test_commitAfterBranchOriginalBranchUnchanged():
+    vcs = Gat()
+    vcs.commit('first commit', 'piet', 'somethings')
+    vcs.createBranch('dev')
+    vcs.checkout('dev')
+    vcs.commit('2nd commit', 'nico', 'other things')
+    vcs.checkout('main')
+    assert(vcs.status() == 'main >> piet : first commit')
+
 def test_log():
     vcs = Gat()
     vcs.commit('first commit', 'piet', 'somethings')
     vcs.commit('2nd commit', 'nico', 'other things')
     assert(vcs.log()=='nico : 2nd commit\npiet : first commit\nsystem : initial commit\n')
+
+def test_logDifferentBranches():
+    vcs = Gat()
+    vcs.commit('first commit', 'piet', 'somethings')
+    vcs.createBranch('dev')
+    vcs.checkout('dev')
+    vcs.commit('2nd commit', 'nico', 'other things')
+    assert(vcs.log() =='nico : 2nd commit\npiet : first commit\nsystem : initial commit\n')
+    vcs.checkout('main')
+    assert(vcs.log() == 'piet : first commit\nsystem : initial commit\n')
+    vcs.commit('3rd','sille','some')
+    assert(vcs.log() == 'sille : 3rd\npiet : first commit\nsystem : initial commit\n')
+    vcs.checkout('dev')
+    assert(vcs.log() ==
+           'nico : 2nd commit\npiet : first commit\nsystem : initial commit\n')
+
 
 def test_find():
     vcs = Gat()
@@ -29,3 +62,19 @@ def test_find():
     vcs.commit('2nd commit', 'nico', 'other things')
     assert(vcs.findCommit('first commit') == True)
     assert(vcs.findCommit('3rd') == False)
+    assert(vcs.findCommit('initial commit') == True)
+
+def test_findCommonCommitBetweenTwoBranches():
+    vcs = Gat()
+    vcs.commit('first commit', 'piet', 'somethings')
+    vcs.createBranch('dev')
+    vcs.checkout('dev')
+    assert(vcs.findCommonCommit('main') == 'piet : first commit')
+    vcs.commit('2nd commit', 'nico', 'other things')
+    assert(vcs.findCommonCommit('main') == 'piet : first commit')
+    vcs.checkout('main')
+    #assert(vcs.findCommonCommit('dev') == 'piet : first commit')
+    vcs.commit('3rd', 'sille', 'things')
+    #assert(vcs.findCommonCommit('dev') == 'piet : first commit')
+    
+    
